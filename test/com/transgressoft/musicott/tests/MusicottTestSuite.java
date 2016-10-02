5ee14fd28356b373c6b664b24cd665c164461265
Otto <octaviospain@gmail.com>
2016-10-02 12:08:01 +0200

    Added test suite

---
+++ test/com/transgressoft/musicott/tests/MusicottTestSuite.java
@@ -0,0 +1,45 @@
+/*
+ * This file is part of Musicott software.
+ *
+ * Musicott software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Musicott library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Musicott. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2015, 2016 Octavio Calleya
+ */
+
+package com.transgressoft.musicott.tests;
+
+import com.transgressoft.musicott.*;
+import com.transgressoft.musicott.model.*;
+import com.transgressoft.musicott.player.*;
+import com.transgressoft.musicott.util.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+import org.junit.runners.Suite.*;
+
+/**
+ * Test suite for the test classes
+ *
+ * @author Octavio Calleya
+ * @version 0.1
+ */
+@RunWith (Suite.class)
+@SuiteClasses ({
+        TrackFieldTest.class,
+        NativePlayerTest.class,
+        Utils_GetAllFilesInFolderTest.class,
+        MainPreferencesTest.class
+})
+public class MusicottTestSuite {
+
+}
